96587f5ca23cc0effd35ac2c4693569b26b630e5
Ryabukhin Stepan <89823401445@yandex.ru>
2020-03-24 11:51:42 +0500

    добавлено распознование скобок

--- __tests__/model.lexal.index.js
+++ __tests__/model.lexal.index.js
@@ -1,19 +1,43 @@
 import TokenBuilder from '../src/model/lexal/TokenBuilder';
 
-test('input number, get number type token', () => {
-  const expression = '1';
+
+test('input operator and number, get operator and number type tokens', () => {
+  const expression = '1+ 2';
   const received = TokenBuilder.getTokens(expression);
   const expected = [
     { type: 'number', value: 1 },
+    { type: 'operator', value: '+', bindingPower: 1, mathFunc: (a, b) => a + b },
+    { type: 'number', value: 2 },
   ];
+  console.log(expected);
   expect(JSON.stringify(received)).toEqual(JSON.stringify(expected));
 });
 
-test('input operator, get operator type token', () => {
-  const expression = '+';
+test('input operator and float number, get operator and number type tokens', () => {
+  const expression = '1.2+ 2.22  -555';
   const received = TokenBuilder.getTokens(expression);
   const expected = [
+    { type: 'number', value: 1.2 },
     { type: 'operator', value: '+', bindingPower: 1, mathFunc: (a, b) => a + b },
+    { type: 'number', value: 2.22 },
+    { type: 'operator', value: '-', bindingPower: 1, mathFunc: (a, b) => a - b },
+    { type: 'number', value: 555 },
+  ];
+  console.log(expected);
+  expect(JSON.stringify(received)).toEqual(JSON.stringify(expected));
+});
+
+test('input operator, number and bracket, get operator, number, bracket type tokens', () => {
+  const expression = '(5 + 3) - 1';
+  const received = TokenBuilder.getTokens(expression);
+  const expected = [
+    { type: 'openBracket', value: '(', bindingPower: 0, mathFunc: null },
+    { type: 'number', value: 5 },
+    { type: 'operator', value: '+', bindingPower: 1, mathFunc: (a, b) => a + b },
+    { type: 'number', value: 3 },
+    { type: 'closedBracket', value: ')', bindingPower: 0, mathFunc: null },
+    { type: 'operator', value: '-', bindingPower: 1, mathFunc: (a, b) => a - b },
+    { type: 'number', value: 1 },
   ];
   console.log(expected);
   expect(JSON.stringify(received)).toEqual(JSON.stringify(expected));
--- src/model/lexal/TokenBuilder.js
+++ src/model/lexal/TokenBuilder.js
@@ -4,13 +4,28 @@ import OperatorToken from "./OperatorToken";
 export default class TokenBuilder {
   static NUMBERS = '0123456789.';
 
-  static OPERATORS = '+-*/';
+  static BRACKETS = '()';
+
+  static OPERATORS = '+-*/()';
+
+  static LETTERALS = '';
+
+  static OPERATORS_TYPE = {
+    '+': 'operator',
+    '-': 'operator',
+    '*': 'operator',
+    '/': 'operator',
+    '(': 'openBracket',
+    ')': 'closedBracket',
+  };
 
   static BINDINGPOWER = {
     '+': 1,
     '-': 1,
     '*': 2,
     '/': 2,
+    '(': 0,
+    ')': 0,
   };
 
   static MATHFUNC = {
@@ -18,20 +33,33 @@ export default class TokenBuilder {
     '-': (a, b) => a - b,
     '*': (a, b) => a * b,
     '/': (a, b) => a / b,
+    '(': null,
+    ')': null,
   };
 
   static getTokens(expression) {
     const expressionWithoutSpace = expression.split('').filter((symbol) => symbol !== ' ');
     const tokens = [];
+    let buffer = [];
+    let currentType = null;
     expressionWithoutSpace.forEach((el) => {
       if (this.NUMBERS.includes(el)) {
-        const token = new NumberToken('number', Number(el));
-        tokens.push(token);
+        buffer.push(el);
       } else if (this.OPERATORS.includes(el)) {
-        const token = new OperatorToken('operator', el, this.BINDINGPOWER[el], this.MATHFUNC[el]);
+        if (buffer.length !== 0) {
+          const token = new NumberToken('number', Number(buffer.join('')));
+          tokens.push(token);
+          buffer = [];
+        }
+        const type = this.OPERATORS_TYPE[el];
+        const token = new OperatorToken(type, el, this.BINDINGPOWER[el], this.MATHFUNC[el]);
         tokens.push(token);
       }
     });
+    if (buffer.length !== 0) {
+      const token = new NumberToken('number', Number(buffer.join('')));
+      tokens.push(token);
+    }
     return tokens;
   }
 }
