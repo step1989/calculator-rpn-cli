c2ad83c528a14d0705df188338f53befcffaa2b5
Ryabukhin Stepan <89823401445@yandex.ru>
2020-03-25 12:20:27 +0500

    удаление лишнего метода в  abstractToke

--- src/lexal/AbstractToken.js
+++ src/lexal/AbstractToken.js
@@ -1,14 +1,9 @@
 export default class AbstractToken {
   constructor(type) {
     this.type = type;
-    // this.value = value;
   }
 
   getType() {
     return this.type;
-  }
-
-  getValue() {
-    return this.value;
   }
 }
